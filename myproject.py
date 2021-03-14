8c00a148e08f92cf5c83856668c2d6c30c2b16e8
Luis Mondragón <luismond.nlp@gmail.com>
2021-03-14 11:07:10 -0600

    Update myproject.py

--- myproject.py
+++ myproject.py
@@ -56,4 +56,4 @@ def get_file(filename):
     return redirect(url_for('uploaded_file', filename=filename))
             
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port='5002')
+	app.run(host='0.0.0.0')
