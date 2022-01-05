c1345a6bbddb74bdde0f013765a0aa5faa0ba91a
Luis Mondragón <luismond@gmail.com>
2022-01-05 05:17:56 -0600

    Update tm2tb_bisentence.py

--- client/tm2tb_bisentence.py
+++ client/tm2tb_bisentence.py
@@ -6,15 +6,20 @@ TM2TB BiSentence class
 import json
 import requests
 from langdetect import detect
-from tm2tb_sentence import Sentence
+import pandas as pd
+from tm2tb import Sentence
+from tm2tb import Ngrams
 
 class BiSentence:
-    'Represents a pair of Sentence objects'
+    """
+    Initialized with a source sentence and a target sentence.
+    Implements methods for comparing a source sentence and a target sentence.
+    It is assumed that the two sentences are translations of each other.
+    """
     def __init__(self, src_sentence, trg_sentence, **kwargs):
         self.src_sentence = src_sentence
         self.trg_sentence = trg_sentence
-
-        'Get optional arguments or use default arguments'
+        self.min_distance = .5
         if 'ngrams_min' in kwargs.keys():
             self.ngrams_min = kwargs.get('ngrams_min')
         else:
@@ -40,34 +45,73 @@ class BiSentence:
         else:
             self.good_tags = ['NOUN','PROPN']
 
-    def get_src_cands(self):
-        'Return candidate ngrams from source sentence'
-        sentence_object = Sentence(self.src_sentence,
-                            lang=self.src_lang,
-                            ngrams_min = self.ngrams_min,
-                            ngrams_max = self.ngrams_max,
-                            good_tags = self.good_tags)
-        src_cands = sentence_object.get_term_candidates()
-        return src_cands
+    def get_candidate_ngrams(self, sentence):
+        """
+        Parameters
+        ----------
+        sentence : string
+            String representing a sentence or paragraph.
 
-    def get_trg_cands(self):
-        'Return candidate ngrams from target sentence'
-        sentence_object = Sentence(self.trg_sentence,
-                            lang=self.trg_lang,
-                            ngrams_min = self.ngrams_min,
-                            ngrams_max = self.ngrams_max,
-                            good_tags = self.good_tags)
-        trg_cands = sentence_object.get_term_candidates()
-        return trg_cands
+        Returns
+        -------
+        candidate_ngrams : list
+            List of substrings representing terms/ngrams/keywords from sentence.
+        """
+        sn = Sentence(sentence,
+                        ngrams_min = self.ngrams_min,
+                        ngrams_max = self.ngrams_max,
+                        good_tags = self.good_tags)
+        
+        sentence = sn.get_clean_sentence()
+        lang = sn.lang
+        ng = Ngrams(sentence, lang=lang)
+        
+        candidate_ngrams = ng.get_candidate_ngrams()
+        
+        #candidate_ngrams = candidate_ngrams[candidate_ngrams['v']==1]
+        candidate_ngrams = candidate_ngrams['joined_ngrams'].tolist()
+        return candidate_ngrams
 
-    def get_terms_similarity(self):
-        'Compare src and trg cands, get similarities'
-        src_cands = self.get_src_cands()
-        trg_cands = self.get_trg_cands()
-        url = 'url'
-        params = json.dumps({
-            'src_cands':src_cands,
-            'trg_cands':trg_cands})
-        response = requests.post(url=url, json=params).json()
-        data = json.loads(response)
-        return data
+    def get_bilingual_ngrams(self):
+        """
+        Get src ngrams.
+        Get trg ngrams.
+        Get their distances.
+        Use conditions to filter them.
+        """
+        src_candidate_ngrams = self.get_candidate_ngrams(self.src_sentence)
+        trg_candidate_ngrams = self.get_candidate_ngrams(self.trg_sentence)
+       
+        def get_src_trg_ngram_distances(src_candidate_ngrams, trg_candidate_ngrams):
+            'Get distances from src ngrams and trg ngrams'
+            url = 'http://0.0.0.0:5000/sim_api'
+            params = json.dumps({
+                'seq1':src_candidate_ngrams,
+                'seq2':trg_candidate_ngrams})
+            response = requests.post(url=url, json=params).json()
+            data = json.loads(response)
+            if len(data)==0:
+                raise ValueError('No similar terms found')
+            return data
+        
+        bilingual_ngrams = get_src_trg_ngram_distances(src_candidate_ngrams,
+                                                       trg_candidate_ngrams)
+        # Make bilingual_ngrams dataframe
+        bilingual_ngrams = pd.DataFrame(bilingual_ngrams)
+        bilingual_ngrams.columns = ['src', 'trg', 'distance']
+        
+        # Group by source, get closest target ngram
+        bilingual_ngrams = pd.DataFrame([df.loc[df['distance'].idxmin()]
+                            for (src_ngram, df) in list(bilingual_ngrams.groupby('src'))])
+        
+        # Group by target, get closest source ngram
+        bilingual_ngrams = pd.DataFrame([df.loc[df['distance'].idxmin()]
+                            for (trg_ngram, df) in list(bilingual_ngrams.groupby('trg'))])
+        
+        # Filter by distance
+        bilingual_ngrams = bilingual_ngrams[bilingual_ngrams['distance'] <= self.min_distance]
+        
+        # Validate bisentence
+        if len(bilingual_ngrams)==0:
+            raise ValueError('No similar terms found')
+        return bilingual_ngrams
