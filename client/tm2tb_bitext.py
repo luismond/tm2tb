9ce5a69c6b6f31cfca3b8633283db1622cb2cdfd
Luis Mondragón <luismond@gmail.com>
2022-01-05 05:19:22 -0600

    Update tm2tb_bitext.py

--- client/tm2tb_bitext.py
+++ client/tm2tb_bitext.py
@@ -9,9 +9,9 @@ from collections import Counter as cnt
 import pandas as pd
 import requests
 
-from tm2tb_sentence import Sentence
-from tm2tb_bisentence import BiSentence
-from tm2tb_bilingual_reader import BilingualReader
+from tm2tb import Sentence
+from tm2tb import BiSentence
+from tm2tb import BilingualReader
 
 class BiText:
     'Represents a collection of BiSentence objects'
@@ -52,9 +52,7 @@ class BiText:
         return bitext
 
     def get_bitext_biterms_precise(self):
-        ''' Get biterms from each bisentence (slower, but more precise)
-            Iterate over all the bitext's rows and get bilingual terms from each one
-        '''
+        'Get biterms from each bisentence (slower, but more precise'
         all_biterms = []
         for i in range(len(self.bitext)):
             try:
@@ -74,10 +72,7 @@ class BiText:
         return all_biterms
 
     def get_bitext_biterms_fast(self):
-        '''Get biterms from document. Faster, but less precise.
-           Get all source candidates and all target candidates,
-           send them to similarity server to compare them.
-        '''
+        'Get biterms from document. Faster, but less precise'
         bitext = self.bitext
         all_src_c = []
         all_trg_c = []
@@ -89,8 +84,13 @@ class BiText:
                 trg_s = Sentence(trg_row)
                 src_c = src_s.get_term_candidates()
                 trg_c = trg_s.get_term_candidates()
+                src_c = pd.concat([src_c])
+                trg_c = pd.concat([trg_c])
+                src_c = src_c['terms'].tolist()
+                trg_c = trg_c['terms'].tolist()
                 all_src_c.append(src_c)
                 all_trg_c.append(trg_c)
+
             except:
                 pass
 
@@ -101,7 +101,7 @@ class BiText:
 
         def get_terms_similarity(src_cands, trg_cands):
             'Compare src and trg cands, get similarities'
-            url = 'url'
+            url = 'http://0.0.0.0:5000/model'
             params = json.dumps({
                 'src_cands':src_cands,
                 'trg_cands':trg_cands})
@@ -113,6 +113,7 @@ class BiText:
 
     def get_closest_biterms(self, biterms):
         'Get top candidates in both directions'
+        # Make dataframe
         biterms = pd.DataFrame(biterms)
         biterms.columns = ['src','trg','distance']
         # Group by source, get the closest target candidate
