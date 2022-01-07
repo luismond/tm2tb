77bb771d5f11db0f7b9ca7da45b29852fd00951d
Luis Mondragón <luismond@gmail.com>
2022-01-07 02:44:21 -0600

    Update bisentence.py

--- tm2tb/bisentence.py
+++ tm2tb/bisentence.py
@@ -7,6 +7,7 @@ import json
 import requests
 import pandas as pd
 from tm2tb import Sentence
+#from tm2tb import SimilarityApi
 
 class BiSentence:
     """
@@ -37,10 +38,14 @@ class BiSentence:
         sn = Sentence(sentence,
                         ngrams_min = self.ngrams_min,
                         ngrams_max = self.ngrams_max,
-                        good_tags = self.good_tags)
+                        good_tags = self.good_tags,
+                        top_n = 8,
+                        diversity=.5,
+                        server_mode='remote')
 
-        non_overlapping_ngrams = sn.get_non_overlapping_ngrams()
-        return [a for (a, b) in non_overlapping_ngrams]
+        #ngrams_to_sentence_distances = sn.get_ngrams_to_sentence_distances()
+        ngrams_to_sentence_distances = sn.get_non_overlapping_ngrams()
+        return [a for (a, b) in ngrams_to_sentence_distances]
 
 
     def get_src_ngrams(self):
@@ -51,26 +56,45 @@ class BiSentence:
         trg_ngrams = self.get_sentence_ngrams(self.trg_sentence)
         return trg_ngrams
 
-    def get_bilingual_ngrams_distances(self):
+    def get_bilingual_ngrams_distances_remote(self):
         """
         Fetches src and trg ngrams.
         Sends them to /sim_api to get their distances.
         """
         src_ngrams = self.get_src_ngrams()
         trg_ngrams = self.get_trg_ngrams()
-        url = 'http://0.0.0.0:5000/sim_api'
-        params = json.dumps({
-            'seq1':src_ngrams,
-            'seq2':trg_ngrams})
+        url = 'http://0.0.0.0:5000/distance_api'
+        params = json.dumps(
+                {'seq1':trg_ngrams,
+                'seq2':src_ngrams,
+                'diversity':.5,
+                'top_n':8,
+                'query_type':'src_ngrams_to_trg_ngrams'})
         
         response = requests.post(url=url, json=params).json()
         bilingual_ngrams_distances = json.loads(response)
-
         return bilingual_ngrams_distances
 
+    def get_bilingual_ngrams_distances_local(self):
+        """
+        Fetches src and trg ngrams.
+        Sends them to sim_api local to get their distances.
+        """
+        src_ngrams = self.get_src_ngrams()
+        trg_ngrams = self.get_trg_ngrams()
+        params = json.dumps(
+            {'seq1':src_ngrams,
+             'seq2':trg_ngrams,
+             'diversity':.5,
+             'top_n':8,
+             'query_type':'src_ngrams_to_trg_ngrams'})
+        
+        response = response = SimilarityApi(params).get_closest_sequence_elements()
+        return response
     
     def filter_bilingual_ngrams(self):
-        bnd = self.get_bilingual_ngrams_distances()
+        #bnd = self.get_bilingual_ngrams_distances_local()
+        bnd = self.get_bilingual_ngrams_distances_remote()
         
         # Make bilingual_ngrams dataframe
         bilingual_ngrams = pd.DataFrame(bnd)
@@ -87,9 +111,8 @@ class BiSentence:
         # Filter by distance
         bilingual_ngrams = bilingual_ngrams[bilingual_ngrams['distance'] <= self.min_distance]
 
-        # # Validate bilingual_ngrams dataframe
+        # # Validate bisentence
         if len(bilingual_ngrams)==0:
             raise ValueError('No similar bilingual_ngrams found!')
             
         return bilingual_ngrams
-    
