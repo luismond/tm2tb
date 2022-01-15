942d1770fbb92d08cf247946a41338efd165d4c4
Luis Mondragón <luismond@gmail.com>
2022-01-15 09:47:22 -0600

    Update spacy_models.py
    add docstring

--- tm2tb/spacy_models.py
+++ tm2tb/spacy_models.py
@@ -1,40 +1,59 @@
 """
 spaCy model selection
-tm2tb needs at least a two spaCy language models to work.
+tm2tb comes pre-packaged with 4 small spaCy language models,
+for English, Spanish, German and French.
 Check the available spaCy language models here: https://spacy.io/models
-
-For example, to install the English and the Spanish language models:
-python -m spacy download es_core_news_sm
-python -m spacy download en_core_web_sm
 """
 
 try:
     import es_core_news_sm
     model_es = es_core_news_sm.load()
-   
-except:
+except ModuleNotFoundError:
     print('No Spanish model found')
+
 try:
     import en_core_web_sm
     model_en = en_core_web_sm.load()
-    
-except:
+except ModuleNotFoundError:
     print('No English model found')
+
 try:
     import de_core_news_sm
     model_de = de_core_news_sm.load()
-    
-except:
+except ModuleNotFoundError:
     print('No German model found')
+
 try:
     import fr_core_news_sm
     model_fr = fr_core_news_sm.load()
-    
-except:
-    print('No French model found') 
+except ModuleNotFoundError:
+    print('No French model found')
 
 def get_spacy_model(lang):
-    'Gets spacy model'
+    """
+    Get spaCy model from one of the supported languages'
+
+    Parameters
+    ----------
+    lang : string
+        Two-character language identifier ('en', 'es', 'de' or 'fr')
+
+    Raises
+    ------
+    ValueError
+        If no installed language models are found.
+
+    Returns
+    -------
+    spacy_model : one of the following spaCy models:
+                    spacy.lang.en.English
+                    spacy.lang.es.Spanish
+                    spacy.lang.de.German
+                    spacy.lang.fr.French
+
+        DESCRIPTION. spaCy language model
+    """
+
     try:
         if lang=='en':
             spacy_model = model_en
@@ -45,5 +64,5 @@ def get_spacy_model(lang):
         if lang=='de':
             spacy_model = model_de
     except Exception:
-        raise ValueError("""No spaCy language models found!\ntm2tb needs at least a two spaCy language models to work.\nCheck the available spaCy language models here: https://spacy.io/models""")
+        raise ValueError("No spaCy language models found!")
     return spacy_model
