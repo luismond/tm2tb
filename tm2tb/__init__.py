b634726080c16fbd28789d3ae5e9f807943a240b
Luis Mondragón <luismond@gmail.com>
2022-01-23 03:33:53 -0600

    Update __init__.py

--- tm2tb/__init__.py
+++ tm2tb/__init__.py
@@ -1,5 +1,8 @@
 __author__ = "Luis Mondragon (luismond@gmail.com)"
-__version__ = '1.0.3'
+__version__ = '1.1.0'
 
 from tm2tb.bitext_reader import BitextReader
-from tm2tb.tm2tb import Tm2Tb, Sentence
+from tm2tb.tm2tb import Tm2Tb
+from tm2tb.tm2tb import Sentence
+from tm2tb.tm2tb import BiSentence
+from tm2tb.tm2tb import BiText
