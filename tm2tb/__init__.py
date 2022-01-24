2d941047713401d1921d837dbee508228966e0a6
Luis Mondragón <luismond@gmail.com>
2022-01-24 03:38:28 -0600

    Update __init__.py

--- tm2tb/__init__.py
+++ tm2tb/__init__.py
@@ -1,8 +1,14 @@
+"""
+TM2TB initialization
+"""
 __author__ = "Luis Mondragon (luismond@gmail.com)"
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 
+from tm2tb.transformer_model import TransformerModel
 from tm2tb.bitext_reader import BitextReader
+from tm2tb.sentence import Sentence
+from tm2tb.bisentence import BiSentence
+from tm2tb.text import Text
+from tm2tb.bitext import BiText
 from tm2tb.tm2tb import Tm2Tb
-from tm2tb.tm2tb import Sentence
-from tm2tb.tm2tb import BiSentence
-from tm2tb.tm2tb import BiText
+trf_model = TransformerModel().load()
