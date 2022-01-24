018951cac8ef7584352064b1ccea7cd8bc9db78c
Luis Mondragón <luismond@gmail.com>
2022-01-24 10:32:00 -0600

    Update text.py

--- tm2tb/text.py
+++ tm2tb/text.py
@@ -1,32 +1,33 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Text class
 """
 from collections import Counter as cnt
 from random import randint
-
 import pandas as pd
 from spacy.tokens import Doc
 from langdetect import detect, LangDetectException
 
-from tm2tb.spacy_models import get_spacy_model
 from tm2tb import trf_model
+from tm2tb.spacy_models import get_spacy_model
 from tm2tb.preprocess import preprocess
 from tm2tb.filter_ngrams import filter_ngrams
+from tm2tb.rank_ngrams import rank_ngrams
+
 pd.options.mode.chained_assignment = None
 
 class Text:
     """
-    Implements fast methods for monolingual terminology extraction.
-    Text is a list of sentences.
+    Class representing a Text object
+    Contains the text sentences
+    Detects the text language
+    Implements a fast spaCy method for tokenizing and POS-tagging
+    Generates term candidates from the text
     """
     def __init__(self, text):
         self.text = text
         self.supported_languages = ['en', 'es', 'de', 'fr']
         self.sentences_clean = self.preprocess_sentences()
         self.lang = self.detect_text_lang()
-        self.candidate_ngrams = self.get_candidate_ngrams()
 
     def preprocess_sentences(self):
         """
@@ -36,7 +37,7 @@ class Text:
         for sentence in self.text:
             try:
                 sentences.append(preprocess(sentence))
-            except:
+            except ValueError:
                 pass
         if len(sentences)==0:
             raise ValueError('No clean sentences!')
@@ -82,30 +83,33 @@ class Text:
         pos_tokens = [(token.text, token.pos_) for token in sc_doc]
         return pos_tokens
 
-    def get_pos_ngrams(self, ngrams_min=1, ngrams_max=2):
-        """
-        Get ngrams from part-of-speech tagged sentences
-        """
+    def get_candidate_ngrams(self, **kwargs):
         pos_tokens = self.get_pos_tokens()
-        pos_ngrams = (zip(*[pos_tokens[i:] for i in range(n)])
-                  for n in range(ngrams_min, ngrams_max+1))
-        return (ng for ngl in pos_ngrams for ng in ngl)
-
-    def get_candidate_ngrams(self,
-                             min_freq=1,
-                             include_pos=None,
-                             exclude_pos=None,
-                             **kwargs):
-        """
-        Get final candidate ngrams from part-of-speech tagged sentences
-        """
-        pos_ngrams = self.get_pos_ngrams(**kwargs)
-        pos_ngrams = [a for a,b in cnt(list(pos_ngrams)).items() if b>=min_freq]
-        candidate_ngrams = filter_ngrams(pos_ngrams,
-                                         include_pos=include_pos,
-                                         exclude_pos=exclude_pos)
+        candidate_ngrams = filter_ngrams(pos_tokens, **kwargs)
         return candidate_ngrams
 
-    def get_embeddings(self):
-        embs = trf_model.encode(self.candidate_ngrams['joined_ngrams'])
-        return embs
+    def get_top_ngrams(self, top_n = None, diversity=.8, return_embs=False, **kwargs):
+        """
+        Embed sentence and candidate ngrams.
+        Calculate the best sentence ngrams using cosine similarity and MMR.
+        """
+        cand_ngrams_df = self.get_candidate_ngrams(**kwargs)
+        joined_ngrams = cand_ngrams_df['joined_ngrams']
+        
+        if top_n is None:
+            top_n = round(len(joined_ngrams)*.85)
+        
+        # Embed document and joined ngrams
+        sent_embs = trf_model.encode(self.sentences_clean)
+        doc_embedding = sum(sent_embs)/len(sent_embs)
+        doc_embedding = doc_embedding.reshape(1, -1)
+        seq2_embeddings = trf_model.encode(joined_ngrams)
+        
+        top_ngrams = rank_ngrams(cand_ngrams_df,
+                                 joined_ngrams,
+                                 doc_embedding,
+                                 seq2_embeddings,
+                                 top_n=100,
+                                 diversity=.8)
+        
+        return top_ngrams
