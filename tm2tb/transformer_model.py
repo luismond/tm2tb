c4e33c497795982a6427a84d0447ae3a0f67ff48
Luis Mondragón <luismond@gmail.com>
2022-01-24 03:37:40 -0600

    Update transformer_model.py

--- tm2tb/transformer_model.py
+++ tm2tb/transformer_model.py
@@ -7,7 +7,7 @@ from sentence_transformers import SentenceTransformer
 
 class TransformerModel:
     def __init__(self):
-        self.path = '/home/user/TM2TB/distiluse-base-multilingual-cased-v1'
+        self.path = 'distiluse-base-multilingual-cased-v1'
 
     def load(self):
         print('Loading sentence transformer model...')
