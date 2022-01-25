04665962b74150bd2aeb0245f4c884e6f890bea2
Luis Mondragón <luismond@gmail.com>
2022-01-25 03:26:19 -0600

    Update tm2tb.py

--- tm2tb/tm2tb.py
+++ tm2tb/tm2tb.py
@@ -2,12 +2,13 @@
 Tm2Tb module.
 Extracts terms from sentences, bisentences, texts and bitexts.
 @author: Luis Mondragon (luismond@gmail.com)
-Last updated on Tue Jan 24 03:55:22 2022
+Last updated on Tue Jan 23 04:55:22 2022
 """
 from tm2tb import Sentence
 from tm2tb import BiSentence
 from tm2tb import Text
 from tm2tb import BiText
+from tm2tb import BitextReader
 
 class Tm2Tb:
     def __init__(self):
@@ -19,18 +20,11 @@ class Tm2Tb:
     def get_terms_from_bisentence(self, bisentence, **kwargs):
         return BiSentence(bisentence).get_top_ngrams(**kwargs)
 
-    def get_terms_from_bitext(self, input_, **kwargs):
-        return BiText(input_).get_top_ngrams(**kwargs)
+    def get_terms_from_bitext(self, bitext_path, **kwargs):
+        bitext = BitextReader(bitext_path).read_bitext()
+        return BiText(bitext).get_top_ngrams(**kwargs)
 
     def get_terms_from_text(self, text_path, **kwargs):
-        text = self.read_text(text_path)
-        return Text(text).get_candidate_ngrams(**kwargs)
-
-    def get_terms_from_two_texts(self, input_, **kwargs):
-        return BiText(input_).get_top_ngrams(**kwargs)
-
-    @staticmethod
-    def read_text(text_path):
         with open(text_path, 'r', encoding='utf8') as file:
-            en_text = file.read().split('\n')
-        return en_text
+            text = file.read().split('\n')
+        return Text(text).get_top_ngrams(**kwargs)
