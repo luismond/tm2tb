c05a4470688ed115765f3c8be0e51973846c0b52
luismond <luismond@gmail.com>
2022-03-02 02:44:00 -0600

    update imports

--- tm2tb/__init__.py
+++ tm2tb/__init__.py
@@ -8,8 +8,6 @@ from tm2tb.transformer_model import TransformerModel
 trf_model = TransformerModel().load()
 from tm2tb.spacy_models import get_spacy_model
 from tm2tb.bitext_reader import BitextReader
-from tm2tb.sentence import Sentence
-from tm2tb.bisentence import BiSentence
-from tm2tb.text import Text
-from tm2tb.bitext import BiText
+from tm2tb.term_extractor import TermExtractor
+from tm2tb.biterm_extractor import BitermExtractor
 from tm2tb.tm2tb import Tm2Tb
