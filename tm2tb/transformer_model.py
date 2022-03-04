73f6c98aa7bbfd1173671404bb0f7586f2276199
luismond <luismond@gmail.com>
2022-03-04 03:49:16 -0600

    Add docstring.

--- tm2tb/transformer_model.py
+++ tm2tb/transformer_model.py
@@ -1,15 +1,47 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Load transformer model
 """
+import os
 from sentence_transformers import SentenceTransformer
 
 class TransformerModel:
-    def __init__(self):
-        self.path = 'distiluse-base-multilingual-cased-v1'
+    """
+    Load a multilingual sentence-transformer model.
+
+    These models were trained on multilingual parallel data.
+
+    They can be used to map different languages to a shared vector space,
+    and are suited for tasks such as bitext extraction,
+    paraphrase extraction, clustering and semantic search.
+
+    They are hosted on the HuggingFace Model Hub.
+    https://huggingface.co/sentence-transformers
+
+    Models compatible with TM2TB:
+
+        LaBSE (best performance, specifically suited for bitext extraction).
+
+        distiluse-base-multilingual-cased-v1
+
+        distiluse-base-multilingual-cased-v2
+
+        paraphrase-multilingual-MiniLM-L12-v2
+
+        paraphrase-multilingual-mpnet-base-v2
+
+    """
+    def __init__(self, model_name):
+        self.path = 'sbert_models'
+        self.model_name = model_name
+        self.model_path = os.path.join(self.path, self.model_name)
 
     def load(self):
-        print('Loading sentence transformer model...')
-        model = SentenceTransformer(self.path)
+        """Load model from path or download it from HuggingFace Model Hub."""
+        if self.model_name in os.listdir(self.path):
+            print('Loading sentence transformer model:\n{}'.format(self.model_name))
+            model = SentenceTransformer(self.model_path)
+        else:
+            model = SentenceTransformer(self.model_name)
+            print('Downloading sentence transformer model:\n{}'.format(self.model_name))
+            model.save(self.model_path)
         return model
