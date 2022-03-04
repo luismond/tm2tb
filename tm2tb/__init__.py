cebeab6583c11cdf02f99afc622ffb92ed331395
luismond <luismond@gmail.com>
2022-03-04 02:55:38 -0600

    Update with latest classes names

--- tm2tb/__init__.py
+++ tm2tb/__init__.py
@@ -2,12 +2,12 @@
 TM2TB initialization
 """
 __author__ = "Luis Mondragon (luismond@gmail.com)"
-__version__ = '1.4.0'
+__version__ = "1.4.0"
 
 from tm2tb.transformer_model import TransformerModel
+
 trf_model = TransformerModel().load()
 from tm2tb.spacy_models import get_spacy_model
 from tm2tb.bitext_reader import BitextReader
 from tm2tb.term_extractor import TermExtractor
 from tm2tb.biterm_extractor import BitermExtractor
-from tm2tb.tm2tb import Tm2Tb
