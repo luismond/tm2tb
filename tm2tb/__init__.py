a9eee9d95a8bcc0be4d4972d65db2356286d54ba
luismond <luismond@gmail.com>
2022-03-04 03:47:14 -0600

    Add lightest transformer model as default

--- tm2tb/__init__.py
+++ tm2tb/__init__.py
@@ -2,11 +2,12 @@
 TM2TB initialization
 """
 __author__ = "Luis Mondragon (luismond@gmail.com)"
-__version__ = "1.4.0"
+__version__ = "1.5.0"
 
 from tm2tb.transformer_model import TransformerModel
 
-trf_model = TransformerModel().load()
+trf_model = TransformerModel("paraphrase-multilingual-MiniLM-L12-v2")
+trf_model.load()
 from tm2tb.spacy_models import get_spacy_model
 from tm2tb.bitext_reader import BitextReader
 from tm2tb.term_extractor import TermExtractor
