f00103accadf173688544db1f7416425efa52e0f
Luis Mondragon <luismond@gmail.com>
2023-11-29 09:43:31 -0600

    improve bitext filenames, add xlsx file

--- test_tm2tb.py
+++ test_tm2tb.py
@@ -371,109 +371,120 @@ def test_bilingual_sentences_lang_codes():
     assert biterms == result
 
 
-def test_bilingual_document():
+BITEXT_RESULT = {
+    'biterm_rank': {
+        0: 1.0,
+        1: 0.604,
+        2: 0.1204,
+        3: 0.0846,
+        4: 0.073,
+        5: 0.0657,
+        6: 0.0641,
+        7: 0.0641,
+        8: 0.0591,
+        9: 0.0529
+        },
+    'frequency': {0: 8, 1: 8, 2: 1, 3: 2, 4: 1, 5: 1, 6: 1, 7: 2, 8: 1, 9: 1},
+    'similarity': {
+        0: 0.9757999777793884,
+        1: 1.0,
+        2: 0.9807000160217285,
+        3: 1.0,
+        4: 0.9623000025749207,
+        5: 0.9563999772071838,
+        6: 0.9542999863624573,
+        7: 1.0,
+        8: 0.9477999806404114,
+        9: 0.9884999990463257
+        },
+    'src_rank': {
+        0: 1.0,
+        1: 0.8603,
+        2: 0.9497,
+        3: 0.4428,
+        4: 0.5109,
+        5: 0.4956,
+        6: 0.4612,
+        7: 0.2903,
+        8: 0.4104,
+        9: 0.3296
+        },
+    'src_tags': {
+        0: ['ADJ', 'NOUN'],
+        1: ['NOUN'],
+        2: ['ADJ', 'NOUN'],
+        3: ['PROPN'],
+        4: ['ADJ', 'NOUN'],
+        5: ['NOUN', 'PROPN'],
+        6: ['ADJ', 'NOUN'],
+        7: ['PROPN'],
+        8: ['ADJ', 'NOUN'],
+        9: ['NOUN']
+        },
+    'src_term': {
+        0: 'giant panda',
+        1: 'panda',
+        2: 'red panda',
+        3: 'Ursidae',
+        4: 'prepared food',
+        5: 'family Ursidae',
+        6: 'taxonomic classification',
+        7: 'China',
+        8: 'common ancestor',
+        9: 'characteristics'
+        },
+    'tgt_rank': {
+        0: 1.0,
+        1: 0.8237,
+        2: 0.9672,
+        3: 0.5008,
+        4: 0.6732,
+        5: 0.5777,
+        6: 0.5878,
+        7: 0.4242,
+        8: 0.5637,
+        9: 0.5051
+        },
+    'tgt_tags': {
+        0: ['PROPN', 'ADJ'],
+        1: ['PROPN'],
+        2: ['PROPN', 'PROPN'],
+        3: ['PROPN'],
+        4: ['NOUN', 'ADJ'],
+        5: ['NOUN', 'PROPN'],
+        6: ['NOUN', 'ADJ'],
+        7: ['PROPN'],
+        8: ['NOUN', 'ADJ'],
+        9: ['NOUN']
+        },
+    'tgt_term': {
+        0: 'panda gigante',
+        1: 'panda',
+        2: 'panda rojo',
+        3: 'Ursidae',
+        4: 'alimentos preparados',
+        5: 'familia Ursidae',
+        6: 'clasificación taxonómica',
+        7: 'China',
+        8: 'ancestro común',
+        9: 'características'
+        }
+    }
+
+
+def test_bilingual_csv():
     """Test bilingual term extraction from English/Spanish document."""
-    path = 'data/panda_bear_english_spanish.csv'
+    path = 'data/test_bitext_en_es.csv'
     bitext = BitextReader(path).read_bitext()
     extractor = BitermExtractor(bitext)
     biterms = extractor.extract_terms()[:10].to_dict()
-    result = {
-        'biterm_rank': {
-            0: 1.0,
-            1: 0.604,
-            2: 0.1204,
-            3: 0.0846,
-            4: 0.073,
-            5: 0.0657,
-            6: 0.0641,
-            7: 0.0641,
-            8: 0.0591,
-            9: 0.0529
-            },
-        'frequency': {0: 8, 1: 8, 2: 1, 3: 2, 4: 1, 5: 1, 6: 1, 7: 2, 8: 1, 9: 1},
-        'similarity': {
-            0: 0.9757999777793884,
-            1: 1.0,
-            2: 0.9807000160217285,
-            3: 1.0,
-            4: 0.9623000025749207,
-            5: 0.9563999772071838,
-            6: 0.9542999863624573,
-            7: 1.0,
-            8: 0.9477999806404114,
-            9: 0.9884999990463257
-            },
-        'src_rank': {
-            0: 1.0,
-            1: 0.8603,
-            2: 0.9497,
-            3: 0.4428,
-            4: 0.5109,
-            5: 0.4956,
-            6: 0.4612,
-            7: 0.2903,
-            8: 0.4104,
-            9: 0.3296
-            },
-        'src_tags': {
-            0: ['ADJ', 'NOUN'],
-            1: ['NOUN'],
-            2: ['ADJ', 'NOUN'],
-            3: ['PROPN'],
-            4: ['ADJ', 'NOUN'],
-            5: ['NOUN', 'PROPN'],
-            6: ['ADJ', 'NOUN'],
-            7: ['PROPN'],
-            8: ['ADJ', 'NOUN'],
-            9: ['NOUN']
-            },
-        'src_term': {
-            0: 'giant panda',
-            1: 'panda',
-            2: 'red panda',
-            3: 'Ursidae',
-            4: 'prepared food',
-            5: 'family Ursidae',
-            6: 'taxonomic classification',
-            7: 'China',
-            8: 'common ancestor',
-            9: 'characteristics'
-            },
-        'tgt_rank': {
-            0: 1.0,
-            1: 0.8237,
-            2: 0.9672,
-            3: 0.5008,
-            4: 0.6732,
-            5: 0.5777,
-            6: 0.5878,
-            7: 0.4242,
-            8: 0.5637,
-            9: 0.5051
-            },
-        'tgt_tags': {
-            0: ['PROPN', 'ADJ'],
-            1: ['PROPN'],
-            2: ['PROPN', 'PROPN'],
-            3: ['PROPN'],
-            4: ['NOUN', 'ADJ'],
-            5: ['NOUN', 'PROPN'],
-            6: ['NOUN', 'ADJ'],
-            7: ['PROPN'],
-            8: ['NOUN', 'ADJ'],
-            9: ['NOUN']
-            },
-        'tgt_term': {
-            0: 'panda gigante',
-            1: 'panda',
-            2: 'panda rojo',
-            3: 'Ursidae',
-            4: 'alimentos preparados',
-            5: 'familia Ursidae',
-            6: 'clasificación taxonómica',
-            7: 'China',
-            8: 'ancestro común',
-            9: 'características'
-            }
-        }
-    assert biterms == result
+    assert biterms == BITEXT_RESULT
+
+
+def test_bilingual_xlsx():
+    """Test bilingual term extraction from English/Spanish document."""
+    path = 'data/test_bitext_en_es.xlsx'
+    bitext = BitextReader(path).read_bitext()
+    extractor = BitermExtractor(bitext)
+    biterms = extractor.extract_terms()[:10].to_dict()
+    assert biterms == BITEXT_RESULT
