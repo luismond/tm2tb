9ad07657308f26e847d26d53ff1f0b9c395b25a2
luismond <luismond.nlp@gmail.com>
2025-11-19 04:07:29 -0600

    refactor saving methods; pass max-rows argument

--- scripts/dataset_builder.py
+++ scripts/dataset_builder.py
@@ -2,38 +2,38 @@
 Build a dataset of silver seq2seq pairs for T5 model.
 
 v3-dataset-build goals: 
-- weed out segments if they are too short or too long
-- identify language of segments, and only process segments that are in the same language pair
 - remove unnecessary arguments from the function
 - utilize term extraction parameters to control the output
 - add a new method to add hard negatives to the dataset
 
 """
-from __future__ import annotations
-import json, random
-from pathlib import Path
-from typing import Iterable, Dict, List, Tuple
+import json
 import argparse
 import ast
+from pathlib import Path
+from typing import Dict, List, Tuple
 from tqdm import tqdm
+import pandas as pd
 from tm2tb.core.io_utils import BitextReader
 from tm2tb.core.biterm_extractor import BitermExtractor 
 
 Example = Dict[str, str]
+
 
 def build_examples(
     input_file: Path,
     src_lang: str,
     tgt_lang: str,
     similarity_min: float = 0.85,
-    span_range: tuple = (1, 2)
+    span_range: Tuple = (1, 2),
+    max_rows: int = 200
 ) -> List[Example]:
     """
-    Produce silver seq2seq pairs for T5 model:
+    Produce silver seq2seq pairs for model training:
     {"src_segment": "...", "tgt_segment": "...", "src_term": "...", "tgt_term": "...", "similarity": "...", "biterm_rank": "...", "lang_pair": "en-es"}
     """
     examples: List[Example] = []
-    bitext = BitextReader(input_file, max_size=200000000).read_bitext()
+    bitext = BitextReader(input_file, max_size=200000000).read_bitext()[:max_rows]
     n = 0
     for src_segment, tgt_segment in tqdm(bitext):
         n += 1
@@ -45,6 +45,7 @@ def build_examples(
             )
         for biterm in biterms:
             examples.append({
+                "idx": n,
                 "src_segment": src_segment,
                 "tgt_segment": tgt_segment,
                 "src_term": biterm.src_term,
@@ -56,9 +57,21 @@ def build_examples(
     return examples
 
 
+def save_examples_as_jsonl(examples, path):
+    with path.open("w", encoding="utf-8") as f:
+        for example in examples:
+            f.write(json.dumps(example, ensure_ascii=False) + "\n")
+
+
+def save_examples_as_csv(examples, path):
+    df = pd.DataFrame(examples)
+    df.to_csv(path, sep='\t', index=False, encoding='utf-8')
+    print(df.head())
+
+
 def main():
     """
-    Main function to build a dataset of silver seq2seq pairs for T5.
+    Main function to build a dataset of silver seq2seq pairs for model training.
     
     Usage: 
     python scripts/dataset_builder.py \
@@ -71,21 +84,24 @@ def main():
     ap.add_argument("--input-file", type=Path, required=True)
     ap.add_argument("--src-lang", required=True)
     ap. add_argument("--tgt-lang", required=True)
+    ap.add_argument("--similarity-min", type=float, default=0.85)
+    ap.add_argument("--span-range", type=ast.literal_eval)
+    ap.add_argument("--max-rows", type=int)
     ap.add_argument("--output-file", type=Path, required=True)
-    ap.add_argument("--span_range", type=ast.literal_eval)
     args = ap.parse_args()
 
     examples = build_examples(
         input_file=args.input_file,
         src_lang=args.src_lang,
         tgt_lang=args.tgt_lang,
-        span_range=args.span_range
+        similarity_min=args.similarity_min,
+        span_range=args.span_range,
+        max_rows=args.max_rows
     )
-    #examples = add_hard_negatives(examples, rate=args.negative_rate)
-    args.output_file.parent.mkdir(parents=True, exist_ok=True)  
-    with args.output_file.open("w", encoding="utf-8") as f:
-        for ex in examples:
-            f.write(json.dumps(ex, ensure_ascii=False) + "\n")
+    
+    save_examples_as_jsonl(examples, args.output_file)
+    save_examples_as_csv(examples, args.output_file.with_suffix('.csv'))
 
+    
 if __name__ == "__main__":
     main()
